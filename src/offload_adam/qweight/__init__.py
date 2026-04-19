b9b5582fb5b239582fd6b1aa188b6315ea020fe6
tascj <tascj0@gmail.com>
2026-04-19 05:53:44 +0000

    Add NF4QWeight (bnb NF4 blockwise, no double-quant)

--- src/offload_adam/qweight/__init__.py
+++ src/offload_adam/qweight/__init__.py
@@ -2,8 +2,9 @@ from .apply import QuantizeReport, quantize_linears
 from .base import QWeightBase
 from .int4 import Int4QWeight
 from .int8 import Int8QWeight
+from .nf4 import NF4QWeight
 
 __all__ = [
-    "QWeightBase", "Int4QWeight", "Int8QWeight",
+    "QWeightBase", "Int4QWeight", "Int8QWeight", "NF4QWeight",
     "quantize_linears", "QuantizeReport",
 ]
