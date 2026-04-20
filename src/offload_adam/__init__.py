466b80e846484281c77373933e283fa965ab6692
tascj <tascj0@gmail.com>
2026-04-20 13:18:26 +0000

    Bump version to 0.2.0

--- src/offload_adam/__init__.py
+++ src/offload_adam/__init__.py
@@ -2,5 +2,5 @@ from .adam import Adam
 from .distributed_offload_adam import DistributedOffloadAdam
 from .offload_adam import OffloadAdam
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["Adam", "OffloadAdam", "DistributedOffloadAdam"]
